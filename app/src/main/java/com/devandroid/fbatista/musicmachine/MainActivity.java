af5a7f645d6b838a37c73703d3463fb79fcdb24a
Fernando Batista <fernandohsbatista@gmail.com>
2018-06-11 04:45:37 -0300

    Primeiro commit

---
+++ app/src/main/java/com/devandroid/fbatista/musicmachine/MainActivity.java
@@ -0,0 +1,32 @@
+package com.devandroid.fbatista.musicmachine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button mDownloadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDownloadButton = findViewById(R.id.btn_download);
+    }
+
+    private void download(){
+        Long endTime = System.currentTimeMillis() + 10 * 1000;
+        while(System.currentTimeMillis() < endTime){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(TAG, "Download terminado");
+    }
+}

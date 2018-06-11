da5e92cc7ba00bbe9b22339b42d4df73accb64a0
Fernando Batista <fernandohsbatista@gmail.com>
2018-06-11 04:53:18 -0300

    Inserçao da primeira Thread

--- app/src/main/java/com/devandroid/fbatista/musicmachine/MainActivity.java
+++ app/src/main/java/com/devandroid/fbatista/musicmachine/MainActivity.java
@@ -3,7 +3,9 @@ package com.devandroid.fbatista.musicmachine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDownloadButton = findViewById(R.id.btn_download);
+        
+        mDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Iniciando o download", Toast.LENGTH_SHORT).show();
+
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        download();
+                    }
+                };
+
+                Thread thread = new Thread(runnable);
+                thread.setName("Download Thread");
+                thread.start();
+
+
+            }
+        });
     }
+
+    
 
     private void download(){
         Long endTime = System.currentTimeMillis() + 10 * 1000;
